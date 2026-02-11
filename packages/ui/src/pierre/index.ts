c6490680851181ce3c45027822a10a29c4154ce9
Adam <2363879+adamdotdevin@users.noreply.github.com>
2026-02-11 11:50:10 -0600

    wip(ui): update pierre diffs

--- packages/ui/src/pierre/index.ts
+++ packages/ui/src/pierre/index.ts
@@ -105,25 +105,16 @@ const unsafeCSS = `
 
 [data-diff-header],
 [data-diff] {
-  [data-separator-wrapper] {
-    margin: 0 !important;
-    border-radius: 0 !important;
-  }
+  [data-separator],
+  [data-separator-content],
   [data-expand-button] {
-    width: 6.5ch !important;
-    height: 24px !important;
-    justify-content: end !important;
-    padding-left: 3ch !important;
-    padding-inline: 1ch !important;
+    height: 24px;
   }
   [data-separator-multi-button] {
-    grid-template-rows: 10px 10px !important;
+    grid-template-rows: 12px 12px !important;
     [data-expand-button] {
       height: 12px !important;
     }
-  }
-  [data-separator-content] {
-    height: 24px !important;
   }
   [data-column-number] {
     background-color: var(--background-stronger);
